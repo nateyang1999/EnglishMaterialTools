ee8f37ede97f0de1a59a719162f3636bfebfe70a
nateyang1999 <b10730007@gapps.ntust.edu.tw>
2022-04-11 16:23:23 +0800

    init

---
+++ translate.py
@@ -0,0 +1,20 @@
+import googletrans
+import sys
+# Init
+translator = googletrans.Translator()
+
+in_filename = sys.argv[1]
+out_filename = "out_"+in_filename
+f = open(in_filename,"r")
+
+with open(out_filename, 'w', encoding='utf-8') as f_out:
+    for sentence in f:
+        s = sentence+""
+        f_out.write(s)
+        out_str = "("
+        out_str += translator.translate(s,dest='zh-tw').text
+        out_str += ")\n"
+        f_out.write(out_str)
+
+print("翻譯完成 輸出檔名:"+out_filename)
+print("(程式使用googletrans套件,時有不精確之處,請務必自行校對)")
